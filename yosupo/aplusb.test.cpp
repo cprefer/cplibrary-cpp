593a191f30a28c967670945be43a9b053d9d88f6
cprefer <contact@cprefer.com>
2023-07-31 10:28:19 +0800

    aplusb

---
+++ yosupo/aplusb.test.cpp
@@ -0,0 +1,11 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cin.tie(nullptr)->sync_with_stdio(false);
+  int a, b;
+  cin >> a >> b;
+  cout << a + b;
+}
