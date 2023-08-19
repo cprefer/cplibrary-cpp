615b105e7f26aed9516185bc4f811471e64b9faa
cprefer <contact@cprefer.com>
2023-08-19 11:33:14 +0800

    z

--- string/z.hpp
+++ string/z.hpp
@@ -1,17 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> z(const string &s) {
+
+// Note : z[0] = 0.
+vector<int> get_z(const string &s) {
   int n = s.size();
   vector<int> z(n);
   for (int i = 1, j = 0; i < n; i += 1) {
-    z[i] = min(z[i - j], max(j + z[j] - i, 0));
-    while (i + z[i] < n and s[z[i]] == s[i + z[i]]) {
+    z[i] = max(min(z[i - j], j + z[j] - i), 0);
+    while (i + z[i] < n and s[i + z[i]] == s[z[i]]) {
       z[i] += 1;
     }
     if (i + z[i] > j + z[j]) {
       j = i;
     }
   }
-  z[0] = n;
   return z;
 }
--- test/yosupo/zalgorithm.test.cpp
+++ test/yosupo/zalgorithm.test.cpp
@@ -1,15 +1,17 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/zalgorithm"
 
 #include <bits/stdc++.h>
-using namespace std;
-
 #include "string/z.hpp"
+
+using namespace std;
 
 int main() {
   cin.tie(nullptr)->sync_with_stdio(false);
   string s;
   cin >> s;
-  for (int zi : z(s)) {
+  auto z = get_z(s);
+  z[0] = s.size();
+  for (int zi : z) {
     cout << zi << " ";
   }
 }
